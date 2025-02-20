e07db4d96a591530ae981983fbbd4154bf1ab883
pmcdsss <pmcds_@hotmail.com>
2025-02-20 07:17:13 -0300

    Finalização da inscrição por indicação

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -4,9 +4,11 @@ import br.com.nlw.events.dto.ErrorMessage;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicatorNotFoundException;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +21,17 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService subscriptionService;
 
-    @PostMapping("/subscription/{prettyName}")
-    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
-        try{
-            SubscriptionResponse response = subscriptionService.createNewSubscription(prettyName, subscriber);
+    @PostMapping({"/subscription/{prettyName}", "/subscription/{prettyName}/{userId}"})
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName,
+                                                @RequestBody User subscriber,
+                                                @PathVariable(required = false) Integer userId) {
+        try {
+            SubscriptionResponse response = subscriptionService.createNewSubscription(prettyName, subscriber, userId);
 
             if (response != null)
                 return ResponseEntity.ok(response);
 
-        } catch (EventNotFoundException e) {
+        } catch (EventNotFoundException | UserIndicatorNotFoundException e ) {
             return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
         } catch (SubscriptionConflictException e) {
             return ResponseEntity.status(409).body(new ErrorMessage(e.getMessage()));
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -3,6 +3,7 @@ package br.com.nlw.events.service;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicatorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -11,6 +12,8 @@ import br.com.nlw.events.repo.SubscriptionRepository;
 import br.com.nlw.events.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
@@ -22,7 +25,7 @@ public class SubscriptionService {
     @Autowired
     private UserRepository userRepository;
 
-    public SubscriptionResponse createNewSubscription(String eventName, User user) {
+    public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId) {
         Event event = eventRepository.findByPrettyName(eventName);
         if (event == null)
             throw new EventNotFoundException("Evento "+ eventName + " não encontrado.");
@@ -31,6 +34,11 @@ public class SubscriptionService {
         if (userExist == null)
             userExist = userRepository.save(user);
 
+        User indicator = userRepository.findById(userId).orElse(null);
+            //Optional<User> indicator = userRepository.findById(userId);
+        if (indicator == null)
+            throw new UserIndicatorNotFoundException("Usuário indicador não existe.");
+
         Subscription subscription = subscriptionRepository.findByEventAndSubscriber(event, userExist);
         if (subscription != null)
             throw new SubscriptionConflictException("Usuário " + user.getName() + " já inscrito no evento " + event.getPrettyName());
@@ -38,9 +46,11 @@ public class SubscriptionService {
         subscription = new Subscription();
         subscription.setEvent(event);
         subscription.setSubscriber(userExist);
+        subscription.setIndication(indicator);
+
         subscriptionRepository.save(subscription);
 
         return new SubscriptionResponse(subscription.getSubscriptionNumber(),
-                                        "http://codecraft.com/"+subscription.getEvent().getPrettyName()+"/"+subscription.getSubscriber().getId());
+                                        "http://codecraft.com/subscription/"+subscription.getEvent().getPrettyName()+"/"+subscription.getSubscriber().getId());
     }
 }
